071e791c2db64fc95b3bd6a1684d46e213fe8fc3
piyush <piyush.camlenio@gmail.com>
2025-12-01 18:24:26 +0530

    changes in the tags api

--- src/routes/admin/tagRoute.js
+++ src/routes/admin/tagRoute.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const { getTagData, getTagById, addTag, updateTag, deleteTag, bulkDelete, softDeleteTag} = require('../../controller/adminController/tagController')
+const express = require("express");
+const {
+  getTagData,
+  getTagById,
+  addTag,
+  updateTag,
+  deleteTag,
+  bulkDelete,
+  softDeleteTag,
+} = require("../../controller/adminController/tagController");
+const upload = require("../../middleware/multerCloudinaryMiddleware");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/tag-data", getTagData);
+router.get("/:id", getTagById);
+router.post("/add-tag", upload.array("images", 2), addTag);
+router.put("/update-tag/:id", upload.array("images", 2), updateTag);
+router.delete("/delete-tag/:id", deleteTag);
+router.delete("/soft-delete-tag/:id", softDeleteTag);
+router.delete("/bulk-delete", bulkDelete);
 
-router.get('/tag-data', getTagData)
-router.get('/:id', getTagById)
-router.post('/add-tag', addTag)
-router.put('/update-tag/:id', updateTag)
-router.delete('/delete-tag/:id', deleteTag)
-router.delete('/soft-delete-tag/:id', softDeleteTag)
-router.delete('/bulk-delete', bulkDelete)
-
-module.exports = router
+module.exports = router;
